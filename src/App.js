5d4f54648f9d5f74ea4df433504b22521860d003
aleksaida <aida1384@mail.ru>
2022-11-12 12:51:23 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import { useEffect, useState } from 'react'
+import video from './activ.mp4'
+
+function App() {
+
+const [activity, setActivity] = useState('')
+
+useEffect(() => {
+  getActivity()
+}, [])
+
+
+const getActivity = async () => {
+  const responce = await fetch(`http://www.boredapi.com/api/activity/`);
+  const data = await responce.json();
+  setActivity(data)    
+}
+
+    
+    return (
+    <div className="App">
+      <div className="container">
+        <video autoPlay muted loop>
+          <source src={video} type="video/mp4" />
+        </video>
+        <h1>If you don't know what to do</h1>
+        <button className="btn" onClick={ () => getActivity() }>Click here</button>
+      </div>
+
+      <div className='activContainer'>
+        <p className='text'>{activity.activity}</p>
+        <p className='text2'><span>Activity type:</span> {activity.type}</p>
+        <p className='text2'><span>Price: </span>$ {activity.price}</p>
+       
+      </div>
+      </div>    
+    
+)
+  }
+
+ 
+
+export default App;
